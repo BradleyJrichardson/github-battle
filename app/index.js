fe7156bc259f7a9f441d87a9cc4c6ddba2d54ca7
BradleyJrichardson <brdl.richardson@gmail.com>
2019-06-06 10:33:32 +1000

    hello world

--- app/index.js
+++ app/index.js
@@ -1,2 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+// Component
+/// state
+// each component can have its own state
+/// lifecycle
+/// UI
+class App extends React.Component {
+  render() {
+    return <div>Hello World</div>;
+  }
+}
