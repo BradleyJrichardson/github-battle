d09afc51b22ad985c8c62a1690e4e2cf0f8f7024
BradleyJrichardson <brdl.richardson@gmail.com>
2019-06-09 11:35:00 +1000

    .

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { FaUserFriends, FaFighterJet, FaTrophy } from "react-icons/fa";
+import {
+  FaUserFriends,
+  FaFighterJet,
+  FaTrophy,
+  FaTimesCircle
+} from "react-icons/fa";
+import PropTypes from "prop-types";
 
 function Instructions() {
   return (
@@ -34,6 +40,7 @@ class PlayerInput extends React.Component {
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleSubmit(event) {
@@ -42,6 +49,11 @@ class PlayerInput extends React.Component {
     this.props.onSubmit(this.state.username);
   }
 
+  handleChange(event) {
+    this.setState({
+      username: event.target.value
+    });
+  }
   render() {
     return (
       <form className="column player" onSubmit={this.handleSubmit}>
@@ -70,12 +82,104 @@ class PlayerInput extends React.Component {
     );
   }
 }
+// whenever you have a controlled component the value in the input field is just going to be whatever is on the local state so in order to update the text of the iput field we need to update the lcal state
+// this is the handle change
+
+PlayerInput.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired
+};
+
+function PlayerPreview({ username, onReset, label }) {
+  return (
+    <div className="column player">
+      <h3 className="player-label">{label}</h3>
+      <div className="row bg-light">
+        <div className="player-info">
+          <img
+            className="avatar-small"
+            src={`https://github.com/${username}.png?size=200`}
+            alt={`Avatar for ${username}`}
+          />
+          <a href={`https://github.com/${username}`} className="link">
+            {username}
+          </a>
+        </div>
+        <button className="btn-clear flex-center" onClick={onReset}>
+          <FaTimesCircle color="rgb(194, 57, 42)" size={26} />
+        </button>
+      </div>
+    </div>
+  );
+}
+// onClick we are executing the function onReset, which in turn
+// executes handlereset which then sets the state
+PlayerPreview.propTypes = {
+  username: PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired
+};
 
 export default class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerOne: null,
+      playerTwo: null
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    // everytime we make a method on the class we need to bind it if it changes a state
+  }
+  handleSubmit(id, player) {
+    this.setState({
+      [id]: player
+    });
+  }
+  handleReset(id) {
+    this.setState({
+      [id]: null
+    });
+  }
   render() {
+    const { playerOne, playerTwo } = this.state;
+
     return (
       <React.Fragment>
         <Instructions />
+
+        <div className="players-container">
+          <h1 className="center-text header-lig">Players</h1>
+          <div className="row space-around">
+            {playerOne === null ? (
+              <PlayerInput
+                label="Player One"
+                // on submit is coming in as a prop to player input
+                onSubmit={player => this.handleSubmit("playerOne", player)}
+              />
+            ) : (
+              <PlayerPreview
+                username={playerOne}
+                label="player one"
+                onReset={() => this.handleReset("playerOne")}
+              />
+            )}
+            {playerTwo === null ? (
+              <PlayerInput
+                label="Player Two"
+                onSubmit={player => this.handleSubmit("playerTwo", player)}
+              />
+            ) : (
+              <PlayerPreview
+                username={playerTwo}
+                label="player two"
+                onReset={() => this.handleReset("playerTwo")}
+              />
+            )}
+          </div>
+        </div>
       </React.Fragment>
     );
   }
